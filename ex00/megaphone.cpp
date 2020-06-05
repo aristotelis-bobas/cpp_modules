f2dc73bf00bacbe0616577f18cb1297102209f7e
Telly <abobas@student.codam.nl>
2020-06-05 17:15:07 +0200

    ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,75 +6,45 @@
 /*   By: abobas <abobas@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/03 14:48:26 by abobas        #+#    #+#                 */
-/*   Updated: 2020/06/03 15:22:22 by abobas        ########   odam.nl         */
+/*   Updated: 2020/06/05 17:04:32 by abobas        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
 #include <iostream>
 
-int     is_lower_case(char c)
+void	print_argument(std::string arg)
 {
-    return (c >= 'a' && c <= 'z');
+	int	j = 0;
+	int	l = arg.length();
+	while (j < l && isspace(arg[j]))
+		j++;
+	for (int i = j; i < l; i++)
+	{
+		if (isspace(arg[i]))
+		{
+			if (!isspace(arg[i + 1]) && arg[i + 1])
+				std::cout << (char)toupper(arg[i]);
+		}
+		else
+			std::cout << (char)toupper(arg[i]);
+	}
 }
 
-int		is_space(char c)
+int		main(int ac, char **av)
 {
-	return (c == ' ' || c == '\t' || c == '\r' || c == '\n' || c == '\a');
-}
-
-char    *capitalize_str(char *arg)
-{
-    int     i;
-
-    i = 0;
-    while (arg[i] != '\0')
-    {
-        if (is_lower_case(arg[i]))
-            arg[i] = arg[i] - 32;
-        i++;
-    }
-    return (arg);
-}
-
-void    print_argument(char *arg)
-{
-    int     i;
-
-    i = 0;
-    while (is_space(arg[i]) && arg[i] != '\0')
-        i++;
-    while (arg[i + 1] != '\0')
-    {
-        if (is_space(arg[i]))
-        {
-            if (!is_space(arg[i + 1]))
-                std::cout << arg[i];
-        }
-        else
-            std::cout << arg[i];
-        i++;
-    }
-    if (!is_space(arg[i]))
-        std::cout << arg[i];
-}
-
-int     main(int ac, char **av)
-{
-    int     i;
-
-    if (ac == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-    else
-    {
-        i = 1;
-        while (i < ac)
-        {
-            print_argument(capitalize_str(av[i]));
-            if (i + 1 != ac)
-                std::cout << ' ';
-            i++;
-        }
-        std::cout << '\n';
-    }
-    return (0);
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			std::string arg(av[i]);
+			print_argument(arg);
+			if (i + 1 != ac)
+				std::cout << ' ';
+		}
+		std::cout << std::endl;
+	}
+	return (0);
 }
