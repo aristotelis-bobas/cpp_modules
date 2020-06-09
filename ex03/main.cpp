289e088d4fc4c521725beb9e9e696aaa7b474444
Telly <abobas@student.codam.nl>
2020-06-09 17:15:48 +0200

    ex03 ex04 ready for vog

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abobas <abobas@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/07 19:00:33 by abobas        #+#    #+#                 */
-/*   Updated: 2020/06/07 19:43:56 by abobas        ########   odam.nl         */
+/*   Updated: 2020/06/09 17:13:49 by abobas        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 #include "ZombieHorde.hpp"
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 
 int		main()
 {
 	srand(time(0));
 	ZombieHorde Horde("Putrid Piece of Shit", 10);
+	std::cout << "\n\n\n\n< I CAN'T HEAR YOU! >\n\n\n\n" << std::endl;
 	Horde.announce();
+	std::cout << "\n\n\n\n< GOOD BYE, LEAVING PROGRAM NOW! >\n\n\n\n" << std::endl;
 	return (0);
 }
