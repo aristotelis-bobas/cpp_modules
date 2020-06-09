72e3b83ac82a3408658d32eca1e4015411a6a9af
Telly <abobas@student.codam.nl>
2020-06-09 16:09:05 +0200

    OOP'ed ex07

--- ex07/SedIsForLosers.hpp
+++ ex07/SedIsForLosers.hpp
@@ -1,34 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   replace.cpp                                        :+:    :+:            */
+/*   SedIsForLosers.hpp                                 :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: abobas <abobas@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2020/06/08 23:57:41 by abobas        #+#    #+#                 */
-/*   Updated: 2020/06/09 16:07:01 by abobas        ########   odam.nl         */
+/*   Created: 2020/06/09 15:00:33 by abobas        #+#    #+#                 */
+/*   Updated: 2020/06/09 15:41:16 by abobas        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "SedIsForLosers.hpp"
-#include <iostream>
+#ifndef SEDISFORLOSERS_HPP
+#define SEDISFORLOSERS_HPP
 
-int			main(int ac, char **av)
+#include <string>
+#include <fstream>
+
+class SedIsForLosers
 {
-	if (ac != 4)
-	{
-		std::cerr << "Invalid argument" << std::endl;
-		return (1);
-	}
-	try
-	{
-		SedIsForLosers NotSed(av[1], av[2], av[3]);
-		NotSed.replace();
-	}
-	catch(char const *error)
-	{
-		std::cerr << error << std::endl;
-		return (1);
-	}
-	return (0);
-}
+	public:
+
+	SedIsForLosers(char const *file, char const *find, char const *replace);
+	void replace();
+	
+	private:
+
+	std::string		file_;
+	std::string		find_;
+	std::string		replace_;
+	std::ifstream	file_in_;
+	std::ofstream	file_out_;
+};
+
+#endif
