4359f53229f52c93852ba35e53d43ff94eb62bb9
Telly <abobas@student.codam.nl>
2020-06-09 11:31:04 +0200

    added class test

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abobas <abobas@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/06 23:47:34 by abobas        #+#    #+#                 */
-/*   Updated: 2020/06/07 19:40:16 by abobas        ########   odam.nl         */
+/*   Updated: 2020/06/09 11:27:34 by abobas        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,28 @@
 
 void	LateToTheParty()
 {
-	std::cout << "\nA roaming zombie appears...\n" << std::endl;
+	Zombie		*LaterToTheParty;
+	ZombieEvent	LateToThePartyTwo;
+	
+	std::cout << "\nTwo roaming zombies appear...\n" << std::endl;
 	usleep(1000 * 1000);
 	Zombie LateToTheParty(std::string("Rotten Corpse"), std::string("Slow Zombie"));
 	LateToTheParty.announce();
+	LateToThePartyTwo.setZombieType("Rotten Corpse");
+	LaterToTheParty = LateToThePartyTwo.newZombie("Super Slow Zombie");
+	LaterToTheParty->announce();
 	usleep(1000 * 1000);
-	std::cout << "\nThe zombie starts eating the remains of the starved horde\n" << std::endl;
+	std::cout << "\nThe zombies start eating the remains of the starved horde\n" << std::endl;
 	for (int i = 0; i < 5; i++)
 	{
-		LateToTheParty.announce("* MUNCHING AND CRUNCHING ON FLESH AND BONES *");
+		LateToTheParty.announce("* MUNCHING ON FLESH AND BONES *");
+		LaterToTheParty->announce("* CRUNCHING ON FLESH AND BONES *");
 		usleep(1000 * 1000);
 	}
-	std::cout << "\nA loud missile draws the zombie's attention..." << std::endl;
+	std::cout << "\nA loud missile draws the attention of the eating zombies..." << std::endl;
 	usleep(1000 * 1000);
 	std::cout << "\n* A HUGE BLAST OBLITERATES EVERYTHING WITHIN A 5 KM RADIUS *\n" << std::endl;
+	delete LaterToTheParty;
 }
 
 void	StarvingHorde(int amount)
